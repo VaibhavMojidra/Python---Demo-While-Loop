4c47b306498bcd925e6207b7d8777d948c9c2e61
VaibhavMojidra <vmojidra25@gmail.com>
2022-09-20 00:38:24 +0530

    While Loop

---
+++ code/While Loop.py
@@ -0,0 +1,9 @@
+#While Loop
+
+n=0 #Initializing counter
+while n<=5:
+    print("Value of n is "+str(n))
+    n+=1
+
+print("Value of n after loop ends is "+str(n))
+#N will be 6 as in last iteration n value become 6 and condition become false and value of n remained 6 only                                            
